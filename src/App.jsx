aac9bbd29c7991bd3f2e7bf93fc49515bd924635
Shruti Verma <shrutive1009@gmail.com>
2025-03-02 17:06:44 +0530

    Created app with prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
 import './App.css';
+import AddTodo from './components/AddTodo/AddTodo';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
-  return <>Todos</>;
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'todo1', isFinished: true },
+    { id: 2, text: 'todo2', isFinished: false },
+    { id: 3, text: 'todo3', isFinished: true },
+  ]);
+
+  function addTodos(todoText) {
+    let nextId = todos.length + 1;
+    setTodos([...todos, { id: nextId, isFinished: false, text: todoText }]);
+  }
+
+  return (
+    <>
+      <AddTodo addTodos={addTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+function AddTodo({ addTodos }) {
+  const [todoText, setTodoText] = useState('');
+
+  return (
+    <>
+      <input
+        placeholder='add your next todo...'
+        onChange={(e) => setTodoText(e.target.value)}
+        value={todoText}
+      />
+      <button
+        onClick={() => {
+          addTodos(todoText);
+          setTodoText('');
+        }}
+      >
+        Submit
+      </button>
+    </>
+  );
+}
+export default AddTodo;
